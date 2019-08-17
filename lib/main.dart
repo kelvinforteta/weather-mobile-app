3008c88642abc6c2f1385244b4afaccc3e731300
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-08-17 22:57:23 +0100

    add search bar

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Weather App',
       home: HomeScreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,10 +6,85 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isSearch = false;
+  String cityName;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  'https://cdn.pixabay.com/photo/2016/08/09/21/54/yellowstone-national-park-1581879_960_720.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            centerTitle: true,
+            title: isSearch ? searchBar() : Container(),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            actions: <Widget>[
+              isSearch ? Container() : searchIcon(),
+            ],
+          ),
+          body: new Container(
+            color: Colors.transparent,
+          ),
+        ),
+      ],
     );
+  }
+
+  void showSearchBar() {}
+
+  IconButton searchIcon() {
+    return IconButton(
+        icon: Icon(
+          Icons.search,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          isSearch = true;
+          setState(() {});
+        });
+  }
+
+  TextField searchBar() {
+    return TextField(
+        autofocus: true,
+        decoration: InputDecoration(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          hintText: 'Enter city name...',
+          hintStyle: TextStyle(color: Colors.white),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+          suffixIcon: IconButton(
+              icon: Icon(
+                Icons.close,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                isSearch = false;
+                setState(() {});
+              }),
+        ),
+        onChanged: (value) {
+          cityName = value.toLowerCase().trim();
+        });
   }
 }
