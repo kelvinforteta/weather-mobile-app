3950ece94c8ecad76957a6914d7655994c9d7038
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-08-17 20:27:48 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/screens/home_screen.dart';
+
+void main() => runApp(WeatherApp());
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      home: HomeScreen(),
+    );
+  }
+}
